dbde388e076cf320f74f9745c4be4558fb42a9ef
Ketil Malde <ketil.malde@imr.no>
2021-08-12 16:03:45 +0200

    more robustness

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 
 USERCONFIG="--build-arg user=${USERNAME} --build-arg uid=${USERID} --build-arg gid=${GROUPID}"
 
-docker build ${USERCONFIG} -t ${IMAGENAME} .
+docker build ${USERCONFIG} -t ${IMAGENAME} $(dirname "$0")
