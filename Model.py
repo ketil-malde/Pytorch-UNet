02228abc16ac29fc698f8f5f502a5b0113f0db51
Ketil Malde <ketil@malde.org>
2021-11-19 15:24:04 +0100

    Update with local config storage in submodules

--- Model.py
+++ Model.py
@@ -11,38 +11,36 @@ USERID=os.getuid()
 GROUPID=os.getgid()
 CWD=os.getcwd()
 USERNAME=pwd.getpwuid(USERID).pw_name
-RUNTIME='' # '--gpus device=0'
 
 # print(USERID, GROUPID, USERNAME)
 
-def docker_run(args=''):
-    os.system(f'docker run --ipc=host {RUNTIME} --rm --user {USERID}:{GROUPID} -v {CWD}:/project -it {USERNAME}-{IMAGENAME} {args}')
-
-def docker_build(path):
-    os.system(f'docker build --build-arg user={USERNAME} --build-arg uid={USERID} --build-arg gid={GROUPID} -t {USERNAME}-{IMAGENAME} {path}')
-
 class Model:
+    def __init__(self, conf):
+        self.config = conf
 
-    def build(self, conf):
+    def docker_run(self, args = ''):
+        os.system(f'docker run --ipc=host {self.config["runtime"]} --rm --user {USERID}:{GROUPID} -v {CWD}:/project -it {USERNAME}-{IMAGENAME} {args}')
+        
+    def build(self):
         '''Build the docker container'''
-        docker_build(conf['modelpath'])
+        os.system(f'docker build --build-arg user={USERNAME} --build-arg uid={USERID} --build-arg gid={GROUPID} -t {USERNAME}-{IMAGENAME} {self.config["modelpath"]}')
 
-    def train(self, conf):
+    def train(self):
         '''Train the network'''
         # if no initial weights: python3 /src/download_weights.py
-        docker_run('python3 /src/train.py')
+        self.docker_run('python3 /src/train.py -s 0.2')
 
-    def check(self, conf):
+    def check(self):
         '''Verify that data is in place and that the output doesn't exist'''
-        docker_run('ls -ltra')
+        self.docker_run('ls -ltra')
 
     def predict(self, target, output):
         '''Run a trained network on the data in target'''
-        docker_run('echo "not implemented yet"')
+        self.docker_run('echo "not implemented yet"')
 
     def test(self):
         '''Run tests'''
-        docker_run('python3 /src/test.py')
+        self.docker_run('python3 /src/test.py')
 
     def status(self):
         '''Print the current training status'''
@@ -53,22 +51,22 @@ class Model:
         # check if test output is present (and report)
         pass
 
-if __name__ == '__main__':
-    import argparse
-    import sys
+# if __name__ == '__main__':
+#     import argparse
+#     import sys
 
-    if sys.argv[1] == 'train':
-        # p = argparse.ArgumentParser(description='Train Mask R-CNN')
-        train()
-    elif sys.argv[1] == 'test':
-        test()
-    elif sys.argv[1] == 'predict':
-        predict()
-    elif sys.argv[1] == 'check':
-        check()
-    elif sys.argv[1] == 'status':
-        status()
-    elif sys.argv[1] == 'build':
-        docker_build()
-    else:
-        error('Usage: {sys.argv[0]} [check,status,train,predict] ...')
+#     if sys.argv[1] == 'train':
+#         # p = argparse.ArgumentParser(description='Train Mask R-CNN')
+#         train()
+#     elif sys.argv[1] == 'test':
+#         test()
+#     elif sys.argv[1] == 'predict':
+#         predict()
+#     elif sys.argv[1] == 'check':
+#         check()
+#     elif sys.argv[1] == 'status':
+#         status()
+#     elif sys.argv[1] == 'build':
+#         docker_build()
+#     else:
+#         error('Usage: {sys.argv[0]} [check,status,train,predict] ...')
